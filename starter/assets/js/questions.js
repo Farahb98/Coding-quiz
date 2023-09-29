22065178b0a9f6e830ddda16b4d4b720736041e2
unknown <farahboujana2@gmail.com>
2023-09-29 02:18:56 +0100

    Added javascript questions

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,56 @@
+const questions = [
+    {
+      question1: "Commonly used data types DO NOT include",
+      choices: [ 
+          "1. Strings",
+           "2.Booleans",
+          "3. Alerts",
+      "4. Numbers"],
+      correctAnswer: 2,
+    },
+  
+    {
+      question2:"Arrays in JavaScript can be used to store _____.",
+      choices: [
+          "1.Numbers and strings",
+          "2.Other Arrays",
+          "3.Booleans",
+          "4. All of the above"],
+      
+      correctAnswer: 3,
+    },
+  {
+    question3:
+    "String values must be enclosed within _____when being assigned to variables.",
+    choices: [
+      "1.Commas",
+      "2. Curly brackets",
+      "3.Quotes",
+      "4.Parentheses"
+    ],
+    correctAnswer: 2,
+  
+  }
+  
+  
+      { 
+        question4: "A very useful tool used durng development and debugging for printing content to the debugger is:",
+      choices: [ 
+          "1. JavaScript",
+           "2.terminal/bash",
+          "3. for loops",
+      "4. console.log"],
+      correctAnswer: 3,
+    },
+    {
+      question5: "Which of the following is a statement that can be used to termonate a loop,switch or label statement?",
+      choices: [ 
+          "1. Break",
+           "2.Stop",
+          "3. Halt",
+      "4. Exit"],
+      correctAnswer: 0,
+    },
+  
+   
+  ];
